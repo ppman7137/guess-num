f678fc6f6254c1b6cfba431a882ec63b79421d73
Marc Huang <ppman7137@gmail.com>
2022-01-28 17:14:41 +0800

    2nd edition

--- guess-num.py
+++ guess-num.py
@@ -8,11 +8,13 @@ Created on Fri Jan 28 15:41:34 2022
 import random
 
 r = random.randint(1, 100)
+i = 0
 while True:
     num = input('Guess a number btn 1~100: ')
     num = int(num)
+    i += 1 #i= i + 1
     if num == r:
-        print("You number is correct!!")
+        #print("You number is correct by", i,"times!!")
         break
     elif num > r:
         print("Your number is larger ")
